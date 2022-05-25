75d076d5435a1a7c3b1bd27494b8b04e145b3fb6
zhangyunan <zyndev@gmail.com>
2022-05-25 20:41:48 +0800

    初始化项目

---
+++ libra-server/src/main/java/io/github/bw/libra/server/LibraServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.bw.libra.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LibraServerApplication.class, args);
+  }
+
+}
---
+++ libra-server/src/test/java/io/github/bw/libra/server/LibraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bw.libra.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
